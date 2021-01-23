78cf68459e63542b4774027f4c2fe2f5819f3161
aman tiwari <amantiwari78632@gmail.com>
2021-01-23 21:17:38 +0530

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+const IndexPage = () => (
+  <Layout>
+
+      <h1>Hello</h1>
+
+  </Layout>
+)
+
+export default IndexPage
