b21622c089781e3523bf36a466674f8de5e4653f
aman tiwari <amantiwari78632@gmail.com>
2021-01-23 21:37:33 +0530

    Updates

---
+++ component---src-pages-index-js-09e8bf0e3b3b86c5f571.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{Bl7J:function(n,e,t){"use strict";var l=t("q1tI"),r=t.n(l);e.a=function(n){var e=n.children;return r.a.createElement("div",null,e)}},RXBc:function(n,e,t){"use strict";t.r(e);var l=t("q1tI"),r=t.n(l),a=t("Bl7J");e.default=function(){return r.a.createElement(a.a,null,r.a.createElement("h1",null,"Hello wolrd 2"))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-09e8bf0e3b3b86c5f571.js.map
