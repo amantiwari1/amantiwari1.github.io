3ef1bf1bba656e83e31aa7a85525c99b575e5a0f
aman tiwari <amantiwari78632@gmail.com>
2021-01-23 21:21:06 +0530

    Updates

---
+++ component---src-pages-404-js-2a7a12683d33abf60dc9.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{Bl7J:function(t,e,n){"use strict";var a=n("q1tI"),u=n.n(a);e.a=function(t){var e=t.children;return u.a.createElement("div",null,e)}},w2l6:function(t,e,n){"use strict";n.r(e);var a=n("q1tI"),u=n.n(a),l=n("Bl7J");e.default=function(){return u.a.createElement(l.a,null,u.a.createElement("h1",null,"404: Not Found"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-2a7a12683d33abf60dc9.js.map
