0d570df5ce4bf6769f7fa08585dd6166b180f063
aman tiwari <amantiwari78632@gmail.com>
2021-02-05 00:42:22 +0530

    added download feature

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,39 +1,4 @@
-
-const data = {
-    "dataanalyst.jpg": {
-        title: "data science certication"
-    },
-    "datamanipulation.jpg": {
-        title: "data science certication"
-    },
-    "datascience.jpg": {
-        title: "data science certication"
-    },
-    "machinelearning.jpg": {
-        title: "data science certication"
-    },
-    "python.jpg": {
-        title: "data science certication"
-    },
-    "pythonprogrammer.jpg": {
-        title: "data science certication"
-    },
-    "cloudarchitecture.jpg": {
-        title: "data science certication"
-    },
-    "cloudengineering.jpg": {
-        title: "data science certication"
-    },
-    "googleitautomation.jpg": {
-        title: "data science certication"
-    },
-    "googleitsupport.jpg": {
-        title: "data science certication"
-    },
-    "k8s.jpg": {
-        title: "data science certication"
-    },
-}
+const data = require('./data.json');
 
 exports.onCreateNode = ({ node, actions }) => {
     const { createNodeField } = actions
@@ -45,6 +10,12 @@ exports.onCreateNode = ({ node, actions }) => {
                 name: `title`,
                 node,
                 value: data[node.base].title,
+              })
+
+              createNodeField({
+                name: `downloadurl`,
+                node,
+                value: data[node.base].downloadurl,
               })
 
         }
--- src/pages/Certification.tsx
+++ src/pages/Certification.tsx
@@ -14,8 +14,10 @@ const Certification = () => {
         {
   allFile(filter: {relativeDirectory: {eq: "Certification"}}) {
     nodes {
+        name
       fields {
         title
+        downloadurl
       }
       childImageSharp {
         gatsbyImageData(placeholder: BLURRED)
@@ -30,14 +32,14 @@ const Certification = () => {
                 data.allFile.nodes.map((data: any) => (
                     <Col xs={12} md={6} lg={4}>
                         <CertificationCardStyle  >
-                            <Row  >
+                            <Row >
                                 <Col lg={12}>
                                     <GatsbyImage image={data.childImageSharp.gatsbyImageData} alt={data.fields.title} />
                                 </Col>
                                 <Col lg={12} >
                                     <h3>{data.fields.title}</h3>
                                     <ButtonDiv>
-                                        <ButtonView  href="https://www.datacamp.com/statement-of-accomplishment/track/94b31f609cb8dfd74f9b158dcb55b4e6155fc6e5?raw=1" download="data">
+                                        <ButtonView href={data.fields.downloadurl} download={data.name}>
                                             Download
                                         </ButtonView>
                                         <ButtonView target="_blank" rel="noopener noreferrer" href="https://www.datacamp.com/statement-of-accomplishment/track/94b31f609cb8dfd74f9b158dcb55b4e6155fc6e5?raw=1" >
